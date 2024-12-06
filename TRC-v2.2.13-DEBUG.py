bfe49a8e310bb2758e62e86647670629d3a7248e
jcau8 <caudwelljl2@gmail.com>
2024-12-06 19:57:37 +1300

    Print translatedCaesar

--- TRC-v2.2.13-DEBUG.py
+++ TRC-v2.2.13-DEBUG.py
@@ -1,4 +1,4 @@
-# Reverse Caesar cipher debug program
+# Transposition Reverse Caesar cipher Debug Program
 import math as m
 
 def outOfRange(char, msg):
@@ -132,94 +132,159 @@ def reverse(message):
     translated = ''
     
     i = len(message) - 1
+    print('Value of i: %s' % i)
     while i >= 0:
         # This adds the character to the translated variable
+        print('Added reversed character: %s' % message[i])
         translated = translated + message[i]
         # i gets reduced to go to the next letter in the string
         i -= 1
+        print('New value of i: %s' % i)
+        
+    print('Final reversed message: %s' % translated)
         
     return translated
     
-def caesar(mode, message, key, alphabet):
+def caesar(code, message, key, alphabet):
+    print('Caesar cipher:')
+    print()
     translated = ''
     # It does this for every character in the message
     for character in message:
+        print('for loop 1c')
         if character in alphabet:
             # Finding the character in the alphabet and recording the number
             letterNum = alphabet.find(character)
+            print('letterNum: %s' % letterNum)
+            
             if mode == 0:
                 letterNum += key
+                print('Mode C encrypt')
+                print('Changing letterNum by: %s' % key)
+                print('New letterNum: %s' % letterNum)
+                print()
             elif mode == 1:
                 letterNum -= key
+                print('Mode C decrypt')
+                print('Changing letterNum by: %s' % key)
+                print('New letterNum: %s' %  letterNum)
+                print()
             
             # Handling the wrap around
             if letterNum > len(alphabet):
+                print('Wrap around 1')
                 letterNum -= len(alphabet)
+                print('New letterNum: %s' % letterNum)
+                print()
             elif letterNum < 0:
+                print('Wrap around 2')
                 letterNum += len(alphabet)
+                print('New letterNum: %s' % letterNum)
+                print()
+                
             # Adding the translated text
             translated = translated + alphabet[letterNum]
+            print('C Adding: %s' % alphabet[letterNum])
+            print()
             
         else:
             # If the translate failed for some reason just add the character anyways
             translated = translated + character
+            print('Failed, adding character anyways: %s' % character)
+            print()
             
+    print('Final Caesar: %s' % translated)
     return translated
     
 def transposition(mode, msg, key):
+    print('Transposition cipher:')
+    print()
     if mode == 0:
         # Encrypt
+        print('Mode T encrypt')
         char = col = 0
+        print('char: %s, col: %s' % (char, col))
         translated = ''
     
         while char < len(message) and col < key:
             if not outOfRange(char, message):
+                print('Adding %s' % message[char])
+                print()
                 translated += message[char]
                 
             char += key
+            print('New char: %s' % char)
+            print()
             
             if outOfRange(char, message):
+                print('Range check 2e')
                 if col < key:
                     col += 1
                     char = col
+                    print('New char; %s, new col: %s' % (char, col))
+                    print()
                     
+        print('Final encrypted transposition output: %s' % translated)
         return translated
         
     if mode == 1:
         # Decrypt
+        print('Mode T decrypt')
         translated = ''
         char = col = 0
+        print('char: %s, col: %s' % (char, col))
         rem = len(msg) % key
+        print('Rem: %s' % rem)
         rows = m.trunc(len(msg) / key)
+        print('Rows: %s' % rows)
         decryptRows = rows + 1 if rem > 0 else rows
+        print('DecryptRows: %s' % decryptRows)
+        print()
         
         
         while len(msg) != len(translated) and col <= decryptRows:
             if outOfRange(char, msg):
+                print('Range check 1d')
                 break
             
             translated += msg[char]
+            print('Adding: %s' % msg[char])
             char += decryptRows
+            print('New char: %s' % char)
+            print()
             
             if outOfRange(char, msg):
+                print('Range check 2d')
                 if col < decryptRows:
                     col += 1
                     char = col
+                    print('New char: %s, col: %s' % (char, col))
+                    print()
                     continue
         
+        print('Final decrypted transposition output: %s' % translated)
         return translated
 
 def translate(key, mode, message, alphabet):
+    print()
     translated = ''
     # If mode is decrypt then reverse the cipher first
     if mode == 1:
+        print('Mode D, reversing message...')
         message = reverse(message)
+        print('Message: %s' % message)
     
-    translated = transposition(mode, caesar(mode, message, key, alphabet), key)
+    translatedCaesar = caesar(mode, message, key, alphabet)
+    print(translatedCaesar)
+    print()
+    translated = transposition(mode, translatedCaesar, key)
             
     if mode == 0:
+        print('Mode E, reversing message...')
         translated = reverse(translated)
+        print('Translated: %s' % translated)
         
+    print('Final fully translated message: %s' % translated)
     return translated # Returning translated text
        
 mode = getMode()
@@ -229,6 +294,6 @@ key = getKey(len(alphabet))
 message = getMessage()
 
 print()
-print('Below is your translated text:')
+print('Below is your translated text:\n\n')
 print(translate(key, mode, message, alphabet))
 
